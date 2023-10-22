5723e88c7ff0c7cc1751ab23060005e920966c26
gauravgoel11 <gaurav.agrwl.11@gmail.com>
2023-10-22 18:03:19 +0530

    adding half code of prefix prefixSum only till making array of prefixSum

--- prefixSum.java
+++ prefixSum.java
@@ -1,5 +1,18 @@
 public class prefixSum {
-    public static void main(String[] args) {
-        
+
+    public static void prefixsum(int arr[]){
+
+        for (int i = 0; i < arr.length-1; i++) {
+            arr[i+1]=arr[i+1]+arr[i];
+            
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {  
+        int arr[]= {2,3,4,5,6,7,8,8};
+        prefixsum(arr);
+
     }
 }
