a6456f5aa3545293842c215e21861f50407c64da
gauravgoel11 <gaurav.agrwl.11@gmail.com>
2023-10-31 22:18:04 +0530

    counting sort incomplete

---
+++ countingSort.java
@@ -0,0 +1,23 @@
+public class countingSort {
+    public static void countingsort(int arr[]){
+       
+
+
+    }
+
+    public static void printarr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[]={11,10,9,8,7};
+        
+        countingsort(arr);
+        printarr(arr);
+        // printarr(arr);
+        
+        
+    }
+}
