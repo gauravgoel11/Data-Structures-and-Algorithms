8877d61528b33a4a237ad9629ea1d031eaa4c41f
gauravgoel11 <gaurav.agrwl.11@gmail.com>
2023-11-04 12:48:20 +0530

    adding a InputArray

---
+++ InputArray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class InputArray {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int len = sc.nextInt();
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++) {
+      arr[i] = sc.nextInt();
+    }
+    sc.close();
+    for (int j = 0; j < len; j++) {
+      System.out.print(arr[j] + " ");
+    }
+  }
+}
