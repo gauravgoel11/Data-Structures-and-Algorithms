dc216b118d96dd5176e8a1dfe9ccb5f6413b4d01
gauravgoel11 <gaurav.agrwl.11@gmail.com>
2023-11-20 16:36:11 +0530

    Adding Substring

--- Strings.java
+++ Strings.java
@@ -1,18 +1,22 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class Strings {
 
   public static void main(String[] args) {
-    // char arr[] = {'a','b','c','d'};
-    // String str = "abcd";
-    // String str2 = new String("xyz");
-    // Scanner sc  = new Scanner(System.in);
-    // String Str3 = sc.nextLine();
-    // System.out.println(Str3);
-    // sc.close();
+    char arr[] = { 'a', 'b', 'c', 'd' };
+    String str = "abcd";
+    String str2 = new String("xyz");
+    Scanner sc = new Scanner(System.in);
+    String Str3 = sc.nextLine();
+    System.out.println(Str3);
+    sc.close();
     String name = "Gaurav";
     String sirname = "Goel";
     String fullname = name + " " + sirname;
+    System.out.println(arr);
+    System.out.println(str);
+    System.out.println(str2);
+    System.out.println(fullname);
     
   }
 }
