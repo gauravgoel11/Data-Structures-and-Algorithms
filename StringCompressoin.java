4b0ed39552dd3f1b25c34cd586d5a6f3c0edfefd
gauravgoel11 <gaurav.agrwl.11@gmail.com>
2023-11-30 12:34:27 +0530

    Adding StringCompressoin

--- StringCompressoin.java
+++ StringCompressoin.java
@@ -1,5 +1,23 @@
 public class StringCompressoin {
-    public static void main(String[] args) {
-        
+
+  public static String compress(String str) {
+    String newstr = "";
+    for (int i = 0; i < str.length(); i++) {
+      Integer count = 1;
+      while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+        count++;
+        i++;
+      }
+      newstr += str.charAt(i);
+      if (count > 1) {
+        newstr += count.toString();
+      }
     }
+    return newstr;
+  }
+
+  public static void main(String[] args) {
+    String str = "aaaabbccd";
+    System.out.println(compress(str));
+  }
 }
