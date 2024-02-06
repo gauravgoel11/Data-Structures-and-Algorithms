f54a20d4c4d1b4b3e8b6b2f558cc9221f5182d42
gauravgoel11 <gaurav.agrwl.11@gmail.com>
2024-02-06 12:27:47 +0530

    adding  TwoSum programm

---
+++ TwoSum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TwoSum {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int len = sc.nextInt();
+    int[] arr = new int[len];
+    arr[0] = sc.nextInt();
+    arr[1] = sc.nextInt();
+    for (int i = 2; i < arr.length; i++) {
+      arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i]+" ");
+    }
+    sc.close();
+  }
+}
