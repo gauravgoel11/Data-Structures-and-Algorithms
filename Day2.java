e1afa166b0e9474be78b7b9b2fde642ae611b363
Gaurav Goel <130895972+gauravgoel11@users.noreply.github.com>
2024-10-08 23:34:42 +0530

    adding code of binary search on day 2

---
+++ Day2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * Day2
+ */
+public class Day2 {
+
+  public static int BinarySearch(int arr[], int key) {
+    int start = 0;
+    int end = arr.length - 1;
+    while (start <= end) {
+      int mid = (start + end) / 2;
+      if (key > arr[mid]) {
+        start = mid + 1;
+      } else if (key < arr[mid]) {
+        end = mid - 1;
+      } else if (key == arr[mid]) {
+        return mid;
+      }
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    int[] arr = new int[num];
+    for (int i = 0; i < num; i++) {
+      arr[i] = sc.nextInt();
+    }
+    int key = sc.nextInt();
+    System.out.println(BinarySearch(arr, key));
+  }
+}
