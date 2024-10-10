0b81fca09082d7c6c1070f40b61183af152ca156
Gaurav Goel <130895972+gauravgoel11@users.noreply.github.com>
2024-10-10 22:06:49 +0530

    day3

---
+++ SubArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SubArray {
+
+  public static void Subarray(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i; j < arr.length; j++) {
+        System.out.print(arr[j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner((System.in));
+    int size = sc.nextInt();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++) {
+      arr[i] = sc.nextInt();
+      Subarray(arr);
+    }
+  }
+}
+// adding a comment
